8a1dc70911a692969c20ce8e9d427d5f86c26e46
S1i <intoghi@gmail.com>
2025-12-19 00:31:18 +1100

    [ / ] Better json storage
    Optimise some variables, changed the results from in config file to in a folder.

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
 const fs = require("fs")
 const fetch = require("node-fetch")
 const chalk = require("chalk")
+const path = require("path")
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 const config = JSON.parse(fs.readFileSync("config.json", "utf8"))
+let taken = JSON.parse(fs.readFileSync(path.join(__dirname, "results", "taken.json"), "utf8"))
+let available = JSON.parse(fs.readFileSync(path.join(__dirname, "results", "available.json"), "utf8"))
 function createString(length,platform) {
-    var result = '';
-    var characters = '';
+    let result = '';
+    let characters = '';
     if (config[platform]["stringLowers"]) {characters += "abcdefghijklmnopqrstuvwxyz"}
     if (config[platform]["stringUppers"]) {characters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"}
     if (config[platform]["stringNumbers"]) {characters += "0123456789"}
     if (config[platform]["stringSpecials"]) {characters += "-"}
-    var charactersLength = characters.length;
-    for (var i = 0; i < length; i++) {
+    let charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     return result;
 }
 function checkWord(platform,word) {
-    let words = config[platform]["words"]
-    if (words["available"][word] || words["taken"][word]) {
+    if (taken[platform]["words"][word] || available[platform]["words"][word]) {
         return false
     } else {
         return true
     }
 }
 function storeWord(platform,word,type) {
-    config[platform]["words"][type][word] = true
-    fs.writeFileSync(`config.json`, JSON.stringify(config, null, "\t"))
+    const find = (type === "taken" ? taken : available)
+    find[platform]["words"][word] = true
+    fs.writeFileSync(`results/${type}.json`, JSON.stringify(find, null, "\t"))
 }
 const functions = {
     "github": async function(username) {
@@ -36,11 +39,13 @@ const functions = {
             await fetch(`https://github.com/${username}`, {})
             .then((res) => {
                 if (res.status == 200) {
-                    console.log(`[+] ${chalk.red("Found used username:")} ${username}`)
+                    if (config["github"]["logTaken"] === true) {
+                        console.log(`[+] ${chalk.red("Found used github username:")} ${username}`)
+                    }
                     storeWord("github",username,"taken")
                     return true
                 } else if (res.status == 404) {
-                    console.log(`[+] ${chalk.green("Found un-used username:")} ${username}`)
+                    console.log(`[+] ${chalk.green("Found un-used github username:")} ${username}`)
                     storeWord("github",username,"available")
                     return true
                 } else {
@@ -52,53 +57,53 @@ const functions = {
         }
     }
 }
-fs.readFile("config.json", async (err,data) => {
-    if (err) {
-        console.log(err)
-    } else if (data) {
-        const parsedData = JSON.parse(data)
-        if (Object.entries(parsedData).length > 0) {
-            for (const platform in parsedData) {
-                if (Object.entries(parsedData[platform]).length > 0) {
-                    if (parsedData[platform]["enabled"]) {
-                        if (parsedData[platform]["words"] == undefined || Object.entries(parsedData[platform]["words"]).length != 2) {
-                            parsedData[platform]["words"] = {"available": {},"taken": {}}
-                            fs.writeFileSync(`config.json`, JSON.stringify(parsedData, null, "\t"))
-                        }
-                        if (functions[platform]) {
-                            if (parsedData[platform]["scanWords"]) {
-                                for (let word of fs.readFileSync("words.json", "utf8").toString().split(/\n+/)) {
-                                    word = word.replace(/\r?\n|\r/g, '')
-                                    if (checkWord(platform,word)) {
-                                        if (!functions[platform](word)) {
-                                            continue
-                                        }
-                                    } else {
+async function start() {
+    if (Object.entries(config).length > 0) {
+        for (const platform in config) {
+            if (Object.entries(config[platform]).length > 0) {
+                if (config[platform]["enabled"]) {
+                    if (taken[platform]["words"] == undefined ) {
+                        taken[platform]["words"] = {}
+                        fs.writeFileSync(path.join(__dirname, "results", "taken.json"), JSON.stringify(taken, null, "\t"), "utf8" )
+                    }
+                    if (available[platform]["words"] == undefined) {
+                        available[platform]["words"] = {}
+                        fs.writeFileSync(path.join(__dirname, "results", "available.json"), JSON.stringify(available, null, "\t"), "utf8" )
+                    }
+                    if (functions[platform]) {
+                        if (config[platform]["scanWords"]) {
+                            for (let word of fs.readFileSync("words.json", "utf8").split(/\n+/)) {
+                                word = word.replace(/\r?\n|\r/g, '')
+                                if (checkWord(platform,word)) {
+                                    if (!await functions[platform](word)) {
                                         continue
                                     }
-                                    await sleep(parsedData[platform]["cooldownTime"] * 1000)
+                                } else {
+                                    continue
                                 }
-                            } else if (parsedData[platform]["scanString"]) {
-                                while (true) {
-                                    const word = createString(parsedData[platform]["stringLength"] || 5, platform)
-                                    if (platform == "github" && (word.startsWith("-") || word.endsWith("-"))) {
-                                        continue
-                                    } else if (checkWord(platform,word)) {
-                                        if (!functions[platform](word)) {
-                                            continue
-                                        }
-                                    }
-                                    await sleep(parsedData[platform]["cooldownTime"] * 1000)
-                                }
+                                await sleep(config[platform]["cooldownTime"] * 1000)
                             }
-                        } else {
-                            console.log(`You have not put a function for the corresponding platform, platform being ${chalk.green(platform)}.`)
+                        } else if (config[platform]["scanString"]) {
+                            while (true) {
+                                const word = createString(config[platform]["stringLength"] || 5, platform)
+                                if (platform == "github" && (word.startsWith("-") || word.endsWith("-"))) {
+                                    continue
+                                } else if (checkWord(platform, word)) {
+                                    if (!await functions[platform](word)) {
+                                        continue
+                                    }
+                                }
+                                await sleep(config[platform]["cooldownTime"] * 1000)
+                            }
                         }
                     } else {
-                        continue
+                        console.log(`You have not put a function for ${chalk.green(platform)}.`)
                     }
+                } else {
+                    continue
                 }
             }
         }
     }
-})
+}
+start()
